d5be87e91d1c8f9334acb424777317e5a07deaab
HQHuy <huynguyen7770@gmail.com>
2024-12-06 18:11:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.secret_key = "sdc"  # Để sử dụng flash messages
 
 '''Kết Nối GG Sheets và GG Drive'''
-SERVICE_ACCOUNT_FILE = os.path.join("config", "SDC2711 Accountant.json")
-
+# SERVICE_ACCOUNT_FILE = os.path.join("config", "SDC2711 Accountant.json")
+SERVICE_ACCOUNT_FILE = json.loads(os.environ['GOOGLE_SHEET_CREDENTIALS'])
 # Các quyền (scopes) cần thiết cho Google Sheets và Google Drive
 SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets',  # Quyền đọc/ghi Google Sheets
